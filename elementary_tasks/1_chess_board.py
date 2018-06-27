a77fa3d9ea2321d915e341799f4fb774c4ebbf5e
Artur <manukian.artur.94@gmail.com>
2018-06-27 12:13:11 +0300

    Добавил print с запросом ввода параметров. Убрал лишние операции конкатенации в функции расчета строк calc_row. Исправил ошибку с -1 в функции вывода шахматной доски print_board.

--- elementary_tasks/1_chess_board.py
+++ elementary_tasks/1_chess_board.py
@@ -6,12 +6,12 @@ class ChessBoard:
     def calc_row(self):
         half_width = self.width // 2
         if self.width % 2 == 0:
-            row_odd = ('*' + ' ') * half_width
-            row_even = (' ' + '*') * half_width
+            row_odd = '* ' * half_width
+            row_even = ' *' * half_width
             self.print_board(row_odd, row_even)
         else:
-            row_odd = ('*' + ' ') * half_width + '*'
-            row_even = (' ' + '*') * half_width + ' '
+            row_odd = '* ' * half_width + '*'
+            row_even = ' *' * half_width + ' '
             self.print_board(row_odd, row_even)
 
     def print_board(self, row_odd, row_even):
@@ -24,10 +24,11 @@ class ChessBoard:
             for i in range(half_height):
                 print(row_odd)
                 print(row_even)
-                if i == half_height:
-                    print(row_odd - 1)
+                if i == half_height - 1:
+                    print(row_odd)
 
 
+print('Введите поочередно высоту и ширину: ')
 height = int(input())
 width = int(input())
 board = ChessBoard(height, width)
