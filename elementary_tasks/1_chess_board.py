9b5238ecf9c52221b6d669df76e7017b9599ee69
Artur <manukian.artur.94@gmail.com>
2018-06-27 11:52:04 +0300

    Initial commit. Task 1 - Chess Board. First version.

---
+++ elementary_tasks/1_chess_board.py
@@ -0,0 +1,34 @@
+class ChessBoard:
+    def __init__(self,  h, w):
+        self.height = h
+        self.width = w
+
+    def calc_row(self):
+        half_width = self.width // 2
+        if self.width % 2 == 0:
+            row_odd = ('*' + ' ') * half_width
+            row_even = (' ' + '*') * half_width
+            self.print_board(row_odd, row_even)
+        else:
+            row_odd = ('*' + ' ') * half_width + '*'
+            row_even = (' ' + '*') * half_width + ' '
+            self.print_board(row_odd, row_even)
+
+    def print_board(self, row_odd, row_even):
+        half_height = self.height // 2
+        if self.height % 2 == 0:
+            for i in range(half_height):
+                print(row_odd)
+                print(row_even)
+        else:
+            for i in range(half_height):
+                print(row_odd)
+                print(row_even)
+                if i == half_height:
+                    print(row_odd - 1)
+
+
+height = int(input())
+width = int(input())
+board = ChessBoard(height, width)
+board.calc_row()
