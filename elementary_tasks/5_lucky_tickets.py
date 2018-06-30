8cefb9943b25d928f7cddd116249b4fb783aca8d
Artur <manukian.artur.94@gmail.com>
2018-07-01 00:31:22 +0300

    Task 6 - Lucky Tickets.

---
+++ elementary_tasks/5_lucky_tickets.py
@@ -0,0 +1,53 @@
+def moscow_counter(f):
+
+    count_moscow = 0
+
+    for line in f:
+        # Преобразование str в int
+        line = list(line[:6])
+        line = [int(i) for i in line]
+
+        # Срезы двух половин номера билета
+        first_half = line[:3]
+        last_half = line[3:6]
+
+        # print(first_half, last_half)
+        # print(sum(first_half), sum(last_half))
+
+        if sum(first_half) == sum(last_half):
+            count_moscow += 1
+
+    return count_moscow
+
+
+def petersburg_counter(f):
+
+    count_petersburg = 0
+
+    for line in f:
+        # Преобразование str в int
+        line = list(line[:6])
+        line = [int(i) for i in line]
+
+        # Генерация списков с четными и нечетными цифрами билетов
+        even_line = [i for i in line if i % 2 == 0]
+        odd_line = [i for i in line if i % 2 != 0]
+
+        # print(even_line, odd_line)
+        # print(sum(even_line), sum(odd_line))
+
+        if sum(even_line) == sum(odd_line):
+            count_petersburg += 1
+
+    return count_petersburg
+
+
+# C:\Users\Artur\Desktop\tickets.txt
+file_dir = input()
+with open(fr'{file_dir}', 'r') as file:
+    mc = moscow_counter(file)
+    print(f'Moscow lucky tickets: {mc}')
+
+with open(fr'{file_dir}', 'r') as file:
+    pc = petersburg_counter(file)
+    print(f'Petersburg lucky tickets: {pc}')
