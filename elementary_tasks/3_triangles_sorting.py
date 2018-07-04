4915dc0c4e27111cd1382955cb5ddd0887e3f16c
Artur <manukian.artur.94@gmail.com>
2018-07-04 15:17:32 +0300

    Task 3. Triangles Sorting. 1. Вынес метод validation() за пределы класса Triangle. Теперь метод возвращает либо True, либо False. 2. Разбиение вводимой пользователем строки на составные части треугольника вынес из метода validation() в отдельную функцию split() за пределы класса Triangle. 3. Теперь в сам класс попадают только валидные данные. 4. Укоротил на одну строку метод sorting() не изменив функционал. 5. В функцию main() добавил обработку исключений для ValueError и IndexError. Теперь они выводят пояснения о вводимых данных. 6. Теперь при запуске без параметров выводится инструкция по использованияю программы. 7. В конструктор класса Triangle добавлена отладочная информация об адресе экзмепляра в памяти.

--- elementary_tasks/3_triangles_sorting.py
+++ elementary_tasks/3_triangles_sorting.py
@@ -2,39 +2,24 @@ from math import sqrt
 from collections import Counter
 
 
-class Triangles:
-    def __init__(self):
-        self.triangle = input('Input triangles data in format: <name>, '
-                              '<side length>, <side length>, <side length>.\n'
-                              'Data must satisfy next conditions:\n'
-                              'a < b + c\n'
-                              'b < a + c\n'
-                              'c < a + b\n')
+class Triangle:
+    def __init__(self, name, side_a, side_b, side_c):
+        self.name = name
+        self.side_a = side_a
+        self.side_b = side_b
+        self.side_c = side_c
 
-    def validation(self):
-        # Проверка на валидность вводимых данных
-        # Если данные валидны, возвращаются имя и стороны треугольника,
-        # иначе вызывает init и метод валидации
-        triangle = list(self.triangle.split(', '))
-        name = triangle[0].lower()
-        a = float(triangle[1])
-        b = float(triangle[2])
-        c = float(triangle[3])
-        if a < (b + c) and b < (a + c) and c < (a + b):
-            return name, a, b, c
-        else:
-            print('Invalid data. The non-existent triangle. Try again.')
-            self.__init__()
-            self.validation()
+        instance_id = id(Triangle)
+        print(f'Instance id: {instance_id}\n')
 
-    @staticmethod
-    def area_calculation(name, a, b, c):
+    def area_calculation(self):
         # Расчет площади треугольника
         # Возвращает площадь по формуле Герона
-        sp = (a+b+c) / 2.0  # semi perimeter
-        area = round(sqrt(sp * (sp-a) * (sp-b) * (sp-c)), 2)
-        triangle = {name: area}
-        return triangle
+        sp = (self.side_a + self.side_b + self.side_c) / 2.0  # semi perimeter
+        area = round(sqrt(sp * (sp - self.side_a) * (sp - self.side_b) *
+                          (sp - self.side_c)), 2)
+        triangle_area = {self.name: area}
+        return triangle_area
 
     @staticmethod
     def do_continue():
@@ -51,8 +36,7 @@ class Triangles:
     def sorting(triangles_dict):
         # Сортирует словарь по значению в порядке убывания площади
         # Возвращает отсортированный словарь
-        triangles_dict = Counter(triangles_dict)
-        triangles_dict = triangles_dict.most_common()
+        triangles_dict = Counter(triangles_dict).most_common()
         triangles_dict = dict(triangles_dict)
         return triangles_dict
 
@@ -64,22 +48,64 @@ class Triangles:
             print(f'{i}. [Triangle {key}]: {sorted_dict[key]} cm')
 
 
+def validation(a, b, c):
+    # Проверка на валидность вводимых данных
+    if a < (b + c) and b < (a + c) and c < (a + b):
+        return True
+    else:
+        return False
+
+
+def split(triangle):
+    # Разделяет входящую строку на составляющие
+    # треугольника: имя и стороны
+    triangle = list(triangle.split(', '))
+    name = triangle[0].lower()
+    side_a = float(triangle[1])
+    side_b = float(triangle[2])
+    side_c = float(triangle[3])
+    return name, side_a, side_b, side_c
+
+
 def main():
     triangles_dict = dict()
-    triangles = Triangles()
     while True:
-        name, a, b, c = triangles.validation()
-        triangle = triangles.area_calculation(name, a, b, c)
-        triangles_dict.update(triangle)
-        go_on = triangles.do_continue()
-        if go_on is True:
-            triangles.__init__()
-        elif go_on is False:
-            sorted_dict = triangles.sorting(triangles_dict)
-            triangles.print_out_data(sorted_dict)
-            break
+        try:
+            triangle = input('Enter triangle data:\n')
+            # Если параметры отсутвуют,
+            # вызывается инструкция по использованию
+            if not triangle:
+                print('1. Enter triangle data in format: <name>, '
+                      '<side length>, <side length>, <side length>.\n'
+                      '2. Data must satisfy next conditions:\n'
+                      'a < b + c\n'
+                      'b < a + c\n'
+                      'c < a + b\n')
+                continue
+
+            name, side_a, side_b, side_c = split(triangle)
+            is_valid = validation(side_a, side_b, side_c)
+
+            if is_valid:
+                triangle = Triangle(name, side_a, side_b, side_c)
+                triangle_area = triangle.area_calculation()
+                triangles_dict.update(triangle_area)
+                is_continue = triangle.do_continue()
+
+                if is_continue:
+                    continue
+                else:
+                    sorted_dict = triangle.sorting(triangles_dict)
+                    triangle.print_out_data(sorted_dict)
+                    break
+            else:
+                print('Invalid data. The non-existent triangle.\n')
+                continue
+        except (ValueError, IndexError):
+            print('Invalid data. Entry must contain 4 values. <side length> '
+                  'must contain only numbers.\n')
+            continue
 
 
 if __name__ == '__main__':
     main()
-
