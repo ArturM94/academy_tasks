5dd6eda8eb2bc41181de6df04acd7d284765c948
Artur <manukian.artur.94@gmail.com>
2018-07-05 15:58:58 +0300

    Task 2. Envelopes Entry. 1. В конструкторе __init__ и функции main() переменные a, b, c, d переименовал в side_a, side_b, side_c, side_d соответственно.

--- elementary_tasks/2_envelopes_analysis.py
+++ elementary_tasks/2_envelopes_analysis.py
@@ -2,13 +2,13 @@ from elementary_tasks.do_continue import do_continue
 
 
 class Envelope:
-    def __init__(self, a, b, c, d):
+    def __init__(self, side_a, side_b, side_c, side_d):
         # От пользователя принимаются параметры конвертов,
         # вызывается функция определения вхождения конверта
-        self.side_a = a
-        self.side_b = b
-        self.side_c = c
-        self.side_d = d
+        self.side_a = side_a
+        self.side_b = side_b
+        self.side_c = side_c
+        self.side_d = side_d
 
         instance_id = id(Envelope)
         print(f'Instance id: {instance_id}\n')
@@ -29,17 +29,17 @@ def main():
     while is_continue:
             print('Enter the sizes of two envelopes (a, b) and (c, d).')
             try:
-                a = float(input('Side a:\n'))
-                b = float(input('Side b:\n'))
-                c = float(input('Side c:\n'))
-                d = float(input('Side d:\n'))
-                if a < 0 or b < 0 or c < 0 or d < 0:
+                side_a = float(input('Side a:\n'))
+                side_b = float(input('Side b:\n'))
+                side_c = float(input('Side c:\n'))
+                side_d = float(input('Side d:\n'))
+                if side_a < 0 or side_b < 0 or side_c < 0 or side_d < 0:
                     raise ValueError
             except ValueError:
                 print('Invalid value. Entries must contain numbers greater '
                       'than zero.\n')
                 continue
-            envelope = Envelope(a, b, c, d)
+            envelope = Envelope(side_a, side_b, side_c, side_d)
             envelope.envelope_entry()
             is_continue = do_continue()
 
