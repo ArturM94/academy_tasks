4abd5ec32a6e7c9636e8693c74e3da3612039b5a
Artur <manukian.artur.94@gmail.com>
2018-07-09 14:04:19 +0300

    Task 8. Fibonacci Range. 1. Refactored fibonacci() function. 2. All functions added in FibonacciRange class. 3. Added entry point. 4. Imported do_continue() function and added in entry point.

--- elementary_tasks/fibonacci_range.py
+++ elementary_tasks/fibonacci_range.py
@@ -1,25 +1,56 @@
-def fibonacci(n):
-    # Считает число Фибоначчи
-    if n == 0:
-        return 0
-    elif n == 1 or n == 2:
-        return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+from elementary_tasks.do_continue import do_continue
 
 
-def fibonacci_generation(s, f):
-    # Генерирует числа Фибоначчи в указанном диапазоне
-    fibonacci_list = []
-    for n in range(s, f + 1):
-        fibonacci_number = fibonacci(n)
-        fibonacci_list.append(fibonacci_number)
-    return fibonacci_list
+class FibonacciRange:
+    def __init__(self, s, f):
+        self.start = s
+        self.finish = f
+
+    @staticmethod
+    def fibonacci(n):
+        """Counts the Fibonacci number for n
+
+        :param n:
+        :return: a
+        """
+        a = 0
+        b = 1
+        for _ in range(n):
+            a, b = b, a + b
+        return a
+
+    def fibonacci_generation(self):
+        """Generates Fibonacci numbers in the specified range
+
+        :return: fibonacci_list
+        """
+        fibonacci_list = []
+        for n in range(self.start, self.finish + 1):
+            fibonacci_number = self.fibonacci(n)
+            fibonacci_list.append(fibonacci_number)
+        return fibonacci_list
 
 
-start = int(input())
-finish = int(input())
-fibonacci_range = fibonacci_generation(start, finish)
-fibonacci_range = [str(_) for _ in fibonacci_range]
-fibonacci_range = ', '.join(fibonacci_range)
-print(fibonacci_range)
+def main():
+    is_continue = True
+    while is_continue:
+        try:
+            start = int(input('Enter the start number of range:\n'))
+            finish = int(input('Enter the finish number of range:\n'))
+            if start > finish:
+                raise ValueError
+        except ValueError:
+            print('Incorrect value. The finish number must be greater than '
+                  'the start number.\n')
+            continue
+        fr = FibonacciRange(start, finish)
+        fibonacci_range = fr.fibonacci_generation()
+        fibonacci_range = [str(_) for _ in fibonacci_range]
+        fibonacci_range = ', '.join(fibonacci_range)
+        print(fibonacci_range)
+        answer = str(input('Do you want to continue? [y / yes]\n'))
+        is_continue = do_continue(answer)
+
+
+if __name__ == '__main__':
+    main()
