909d807574ec35afb90400127e2f72dbd0cd13eb
Artur <manukian.artur.94@gmail.com>
2018-07-09 14:12:34 +0300

    Added unit-test for do_continue.py.

---
+++ elementary_tasks/tests/do_continue/test_do_continue.py
@@ -0,0 +1,48 @@
+import unittest
+from elementary_tasks import do_continue
+
+
+class DoContinueTest(unittest.TestCase):
+    """do_continue tests"""
+    def setUp(self):
+        """Set up for test"""
+        print('\nSet up for [' + self.shortDescription() + ']')
+        print(self)
+
+    def tearDown(self):
+        """Tear down for test"""
+        print('Tear down for [' + self.shortDescription() + ']\n')
+
+    def test_do_continue_y_lower_case(self):
+        """do_continue 'y' input function test"""
+        result = do_continue.do_continue('y')
+        self.assertTrue(result)
+
+    def test_do_continue_y_upper_case(self):
+        """do_continue 'Y' input function test"""
+        result = do_continue.do_continue('Y')
+        self.assertTrue(result)
+
+    def test_do_continue_yes_lower_case(self):
+        """do_continue 'yes' function test"""
+        result = do_continue.do_continue('yes')
+        self.assertTrue(result)
+
+    def test_do_continue_yes_upper_case(self):
+        """do_continue 'YES' function test"""
+        result = do_continue.do_continue('YES')
+        self.assertTrue(result)
+
+    def test_do_continue_yes_mixed_case(self):
+        """do_continue 'Yes' function test"""
+        result = do_continue.do_continue('Yes')
+        self.assertTrue(result)
+
+    def test_do_continue_false_string(self):
+        """do_continue 'false string' function test"""
+        result = do_continue.do_continue('something weird')
+        self.assertFalse(result)
+
+
+if __name__ == '__main__':
+    unittest.main()
