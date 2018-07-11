3c04b7d692710084acb8b99fc00636d29a331a42
Artur <manukian.artur.94@gmail.com>
2018-07-11 17:08:42 +0300

    Added test for Chessboard.

---
+++ elementary_tasks/tests/chessboard/test_chessboard.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+
+from elementary_tasks.chessboard import ChessBoard
+
+
+class TestChessboard(TestCase):
+    """ChessBoard tests"""
+    def setUp(self):
+        """Set up for test"""
+        print('\nSet up for [' + self.shortDescription() + ']')
+        print(self)
+
+    def tearDown(self):
+        """Tear down for test"""
+        print('Tear down for [' + self.shortDescription() + ']\n')
+
+    def test_even_row_generation(self):
+        """Row Generation method test"""
+        board = ChessBoard(5, 8)
+        result = board.row_generation()
+        self.assertEqual(result, ('■□■□■□■□', '□■□■□■□■'))
+
+    def test_odd_row_generation(self):
+        """Row Generation method test"""
+        board = ChessBoard(5, 7)
+        result = board.row_generation()
+        self.assertEqual(result, ('■□■□■□■', '□■□■□■□'))
+
+
+if __name__ == '__main__':
+    TestChessboard()
