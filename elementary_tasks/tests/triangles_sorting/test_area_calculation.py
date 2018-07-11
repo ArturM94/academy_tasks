5899c774ab3bf3349dc91efbe4fa7b5f2fbb4f90
Artur <manukian.artur.94@gmail.com>
2018-07-11 12:17:47 +0300

    Added 2 tests for area_calculation() function in Triangles Sorting.

--- elementary_tasks/tests/triangles_sorting/test_area_calculation.py
+++ elementary_tasks/tests/triangles_sorting/test_area_calculation.py
@@ -14,7 +14,19 @@ class TestAreaCalculation(TestCase):
         """Tear down for test"""
         print('Tear down for [' + self.shortDescription() + ']\n')
 
-    def test_area_calculation(self):
+    def test_area_calculation_for_float_values(self):
+        """Area Calculation function test"""
+        triangle = Triangle('triangle', 3.7, 4.2, 6.5)
+        result = triangle.area_calculation()
+        self.assertEqual(result, {'triangle': 7.27})
+
+    def test_area_calculation_for_int_values(self):
+        """Area Calculation function test"""
+        triangle = Triangle('triangle', 3.7, 4.2, 6.5)
+        result = triangle.area_calculation()
+        self.assertEqual(result, {'triangle': 7.27})
+
+    def test_area_calculation_for_mixed_values(self):
         """Area Calculation function test"""
         triangle = Triangle('triangle', 3.7, 4.2, 6.5)
         result = triangle.area_calculation()
