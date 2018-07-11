1b74e42612c9b5afadacc905f14e506000ee4b98
Artur <manukian.artur.94@gmail.com>
2018-07-11 12:20:03 +0300

    Added test for sorting() function in Triangles Sorting.

---
+++ elementary_tasks/tests/triangles_sorting/test_sorting.py
@@ -0,0 +1,49 @@
+from unittest import TestCase
+
+from elementary_tasks.triangles_sorting import sorting
+
+
+class TestSorting(TestCase):
+    """Sorting tests"""
+    def setUp(self):
+        """Set up for test"""
+        print('\nSet up for [' + self.shortDescription() + ']')
+        print(self)
+
+    def tearDown(self):
+        """Tear down for test"""
+        print('Tear down for [' + self.shortDescription() + ']\n')
+
+    def test_sorting_for_float_areas(self):
+        """Sorting function test"""
+        unsorted_triangles = {'triangle 1': 7.27,
+                              'triangle 2': 2.9,
+                              'triangle 3': 8.74}
+        result = sorting(unsorted_triangles)
+        self.assertEqual(result, {'triangle 3': 8.74,
+                                  'triangle 1': 7.27,
+                                  'triangle 2': 2.9})
+
+    def test_sorting_for_int_areas(self):
+        """Sorting function test"""
+        unsorted_triangles = {'triangle 1': 7,
+                              'triangle 2': 2,
+                              'triangle 3': 8}
+        result = sorting(unsorted_triangles)
+        self.assertEqual(result, {'triangle 3': 8,
+                                  'triangle 1': 7,
+                                  'triangle 2': 2})
+
+    def test_sorting_for_mixed_areas(self):
+        """Sorting function test"""
+        unsorted_triangles = {'triangle 1': 7,
+                              'triangle 2': 2.9,
+                              'triangle 3': 8.74}
+        result = sorting(unsorted_triangles)
+        self.assertEqual(result, {'triangle 3': 8.74,
+                                  'triangle 1': 7,
+                                  'triangle 2': 2.9})
+
+
+if __name__ == '__main__':
+    TestSorting()
