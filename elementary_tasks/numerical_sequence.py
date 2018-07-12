5f90ab49d0e5d098852cd935c62482cb4304474b
Artur <manukian.artur.94@gmail.com>
2018-07-12 15:01:54 +0300

    Refactored numerical_sequence.py.

--- elementary_tasks/numerical_sequence.py
+++ elementary_tasks/numerical_sequence.py
@@ -1,44 +1,59 @@
-class NaturalNumbers:
-    def __init__(self):
-        self.n = int(input())
-        self.i = 1
-        self.sequence = []
+from elementary_tasks.do_continue import do_continue
+
+
+class SequenceOfNaturalNumbers:
+    def __init__(self, limit_number):
+        """Initialisation method
+
+        :param limit_number: -- Entered natural number
+        """
+        self.limit_number = limit_number
 
     def sequence_generation(self):
-        # Генерирует список последовательности натуральных чисел,
-        # квадрат которых меньше заданного n
-        while self.i ** 2 < self.n:
-            self.sequence.append(self.i)
-            self.i += 1
+        """Sequence generator
 
-        return self.sequence
+        Generates sequence list of natural numbers
+        which square less than limit_number
 
-    # Поскольку метод output_generation не использует экземпляр,
-    # то ему не нужно принимать аргумент self. Следовательно,
-    # метод нужно объявить статическим, либо вывести за пределы класса
-    @staticmethod
-    def output_generation(sequence):
-        # Простая функция
-        # Однострочный вывод последовательности через запятую
+        :return: sequence -- Sequence of natural numbers
+        """
+        sequence = []
+        natural_number = 1  # First natural number
 
-        # Вариант 1
-        # for _ in sequence:
-        #     if _ == sequence[-1]:
-        #         print(_)
-        #     else:
-        #         print(_, end=', ')
-
-        # Вариант 2
-        sequence = [str(_) for _ in sequence]
-        sequence = ', '.join(sequence)
+        while natural_number ** 2 < self.limit_number:
+            sequence.append(natural_number)
+            natural_number += 1
 
         return sequence
 
 
+def output_generation(sequence):
+    """Output generator
+
+    Generates single-line output sequence separated by a comma
+
+    :param sequence: -- Sequence of natural numbers
+    :return: sequence -- Same sequence with a commas
+    """
+    sequence = [str(_) for _ in sequence]
+    sequence = ', '.join(sequence)
+    return sequence
+
+
 def main():
-    natural_numbers = NaturalNumbers()
-    sequence = natural_numbers.sequence_generation()
-    print(natural_numbers.output_generation(sequence))
+    is_continue = True
+
+    while is_continue:
+        try:
+            limit_number = int(input('Enter limit number for sequence:\n'))
+        except ValueError:
+            print('Invalid value. Entry must contain only integer number.\n')
+            continue
+        sequence = SequenceOfNaturalNumbers(limit_number)
+        sequence = sequence.sequence_generation()
+        print(output_generation(sequence))
+        answer = str(input('Do you want to continue? [y / yes]\n'))
+        is_continue = do_continue(answer)
 
 
 if __name__ == '__main__':
