f990dc971f063708c27af687aec1a4395905ff16
Artur <manukian.artur.94@gmail.com>
2018-07-15 18:32:15 +0300

    Created __init__.py for elementary_tasks module.

---
+++ elementary_tasks/__init__.py
@@ -0,0 +1,19 @@
+# __all__ = ['chessboard',
+#            'envelopes_analysis',
+#            'triangles_sorting',
+#            'file_parser',
+#            'number_to_word',
+#            'lucky_tickets',
+#            'numerical_sequence',
+#            'fibonacci_range',
+#            'do_continue']
+
+from .chessboard import ChessBoard
+from .envelopes_analysis import Envelope
+from .triangles_sorting import Triangle
+from .file_parser import read_file, replace_line
+from .number_to_word import WordNumber
+from .lucky_tickets import moscow_counter, petersburg_counter
+from .numerical_sequence import SequenceOfNaturalNumbers, output_generation
+from .fibonacci_range import FibonacciRange
+from .do_continue import do_continue
