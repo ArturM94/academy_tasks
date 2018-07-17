6e7da771621e9b18c841d177134ec50f29a893e2
Artur <manukian.artur.94@gmail.com>
2018-07-17 13:36:48 +0300

    Task 2. Envelopes Analysis. 1. Refactored __init__ method in Envelope class. Now it gets sides for only one envelope. 2. Refactored main() function. Now creates two instance of envelope. 3. Refactored envelope_entry() method. Now it gets two instance of envelope and works with instance fields. Method taken out of Envelope class.

--- elementary_tasks/envelopes_analysis.py
+++ elementary_tasks/envelopes_analysis.py
@@ -2,32 +2,33 @@ from elementary_tasks.do_continue import do_continue
 
 
 class Envelope:
-    def __init__(self, side_a, side_b, side_c, side_d):
+    def __init__(self, first_side, second_side):
         """Initialisation method
 
-        :param side_a: -- Side a for first envelope
-        :param side_b: -- Side b for first envelope
-        :param side_c: -- Side c for second envelope
-        :param side_d: -- Side d for second envelope
+        :param first_side: -- First side envelope
+        :param second_side: -- Second side envelope
         """
-        self.side_a = side_a
-        self.side_b = side_b
-        self.side_c = side_c
-        self.side_d = side_d
+        self.first_side = first_side
+        self.second_side = second_side
 
-    def envelope_entry(self):
-        """Compares envelopes
 
-        Compares sides of two envelopes in pairs
+def envelope_entry(envelope1, envelope2):
+    """Compares envelopes
 
-        :return: -- String result of comparision
-        """
-        if (self.side_a > self.side_c) and (self.side_b > self.side_d):
-            return 'The second envelope can be nested in the first.\n'
-        elif (self.side_a < self.side_c) and (self.side_b < self.side_d):
-            return 'The first envelope can be nested in the second.\n'
-        else:
-            return 'None of the envelopes can be nested in another.\n'
+    Compares sides of two envelopes in pairs
+
+    :param envelope1: -- First envelope
+    :param envelope2: -- Second envelope
+    :return: -- String result of comparision
+    """
+    if (envelope1.first_side > envelope2.first_side) and \
+            (envelope1.second_side > envelope2.second_side):
+        return 'The second envelope can be nested in the first.\n'
+    elif (envelope1.first_side < envelope2.first_side) and \
+            (envelope1.second_side < envelope2.second_side):
+        return 'The first envelope can be nested in the second.\n'
+    else:
+        return 'None of the envelopes can be nested in another.\n'
 
 
 def main():
@@ -39,14 +40,16 @@ def main():
                 side_b = float(input('Side b:\n'))
                 side_c = float(input('Side c:\n'))
                 side_d = float(input('Side d:\n'))
+                # Additional validation
                 if side_a < 1 or side_b < 1 or side_c < 1 or side_d < 1:
                     raise ValueError
             except ValueError:
                 print('Invalid value. Entries must contain numbers greater '
                       'than zero.\n')
                 continue
-            envelope = Envelope(side_a, side_b, side_c, side_d)
-            entry_result = envelope.envelope_entry()
+            envelope1 = Envelope(side_a, side_b)
+            envelope2 = Envelope(side_c, side_d)
+            entry_result = envelope_entry(envelope1, envelope2)
             print(entry_result)
             answer = str(input('Do you want to continue? [y / yes]\n'))
             is_continue = do_continue(answer)
