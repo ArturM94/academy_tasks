58d0b6ea5af5d2bc39119ef65948a79d82abb100
Artur <manukian.artur.94@gmail.com>
2018-07-18 17:02:25 +0300

    Refactored exception handler in file_parser.py and added do_continue.py module.

--- elementary_tasks/file_parser.py
+++ elementary_tasks/file_parser.py
@@ -1,3 +1,6 @@
+from elementary_tasks import do_continue
+
+
 def read_file(f_dir, string):
     with open(fr'{f_dir}', 'r') as file:
         counter = 0
@@ -24,41 +27,52 @@ def replace_line(f_dir, str_search, str_replace):
 
 
 def main():
-    try:
-        while True:
-            mode = input('[1] - Count of string occurrences in a text file.\n'
-                         '[2] - Replacement of one string for another in a text '
-                         'file.\n'
-                         'Select the mode. [1 / 2]\n')
-            # C:\Users\Artur\Desktop\parsing1.txt
-            # 402795
-            if mode == '1':
-                file_dir = input('File directory:\n')
-                string_for_counting = input('String for counting:\n')
+    is_continue = True
+
+    while is_continue:
+        mode = input('[1] - Count of string occurrences in a text file.\n'
+                     '[2] - Replacement of one string for another in a text '
+                     'file.\n'
+                     'Select the mode. [1 / 2]\n')
+        # C:\Users\Artur\Desktop\parsing1.txt
+        # 402795
+        if mode == '1':
+            file_dir = input('File directory:\n')
+            string_for_counting = input('String for counting:\n')
+            try:
                 count = read_file(file_dir, string_for_counting)
-                print(count)
-            # C:\Users\Artur\Desktop\parsing2.txt
-            # Самым известным «рыбным» текстом является знаменитый Lorem ipsum.
-            elif mode == '2':
-                file_dir = input('File directory:\n')
-                string_for_searching = str(input('String for searching:\n'))
-                string_for_replacing = str(input('String for changing:\n'))
-                replace_line(file_dir, string_for_searching, string_for_replacing)
-            elif not mode:
-                print('FILE PARSER\n'
-                      '1. Enter the mode of the file parser.\n'
-                      '2. Enter the requested data\n'
-                      '    for mode 1:\n'
-                      '    - file directory\n'
-                      '    - string for counting\n'
-                      '    for mode 2:\n'
-                      '    - file directory\n'
-                      '    - string for counting\n'
-                      '    - string for replacing\n')
-            else:
-                print('Incorrect input. Enter must contain number of mode.\n')
-    except FileNotFoundError:
-        print('Not found file or directory.')
+            except FileNotFoundError:
+                print('Not found file or directory.\n')
+                continue
+            print(count)
+        # C:\Users\Artur\Desktop\parsing2.txt
+        # Самым известным «рыбным» текстом является знаменитый Lorem ipsum.
+        elif mode == '2':
+            file_dir = input('File directory:\n')
+            string_for_searching = str(input('String for searching:\n'))
+            string_for_replacing = str(input('String for changing:\n'))
+            try:
+                replace_line(file_dir, string_for_searching,
+                             string_for_replacing)
+            except FileNotFoundError:
+                print('Not found file or directory.\n')
+                continue
+        elif not mode:
+            print('FILE PARSER\n'
+                  '1. Enter the mode of the file parser.\n'
+                  '2. Enter the requested data\n'
+                  '    for mode 1:\n'
+                  '    - file directory\n'
+                  '    - string for counting\n'
+                  '    for mode 2:\n'
+                  '    - file directory\n'
+                  '    - string for counting\n'
+                  '    - string for replacing\n')
+        else:
+            print('Incorrect input. Enter must contain number of mode.\n')
+
+        answer = str(input('Do you want to continue? [y / yes]\n'))
+        is_continue = do_continue(answer)
 
 
 if __name__ == '__main__':
